e76168eeca392b33e23c42f9770ad36ca79ee172
EdgarGaribayAlvarado <escobadoo97@gmail.com>
2023-07-26 20:04:07 -0600

    se añadio la conexion
    añadi el servidor de nodejs con express, y  hice un metodo get con un parametro de salida json

--- APIRest-NodeJS/Leccion 5/ApiRestCurso/index.js
+++ APIRest-NodeJS/Leccion 5/ApiRestCurso/index.js
@@ -1,4 +1,34 @@
-const{ Conection }=require("./Database/Conection"); //se guarda la funcion en otra constante llamada Conection y se le indica la ruta de donde esta
-console.log("conectando la base de datos...");
+const{ Conection }=require("./Database/Conection");//se guarda la funcion en otra constante llamada Conection y se le indica la ruta de donde esta
+const express=require("express");// se guardan las funciones de express
+const cors=require("cors");//paquete para prevenir errores
+ console.log("conectando la base de datos...");
 
 Conection();// se llama el metodo
+
+
+//----------------------Servidor de node-----------------------------------//
+
+const app=express(); //todos los metodos para el servidor
+const puerto=3900;
+//configurar cors
+app.use(cors());
+
+//convertir body a objeto js
+app.use(express.json());
+
+//Crear rutas
+app.get("/probando",(req,res)=>{
+    console.log("Se ha ejecutado el endpoint probando");
+return res.status(200).json({
+    curso:"Master en nodejs",
+    autor:"Goku",
+    url:"www.facebook.com/104568395"
+})
+});
+
+//Crear servidor y ver peticiones http
+
+app.listen(puerto,()=>{
+    console.log("Servidor corriendo en el puerto: "+puerto);
+});
+//--------------------------------------------.---------------------------//
